ab8bd35e1a39348fc0ccddd6481ea583dec3f9c9
foyzul09 <141473705+foyzul09@users.noreply.github.com>
2025-07-19 14:16:50 +0600

    Update deleteindex.c
    in this code you delete any desired element you want after putting the numbers of the elements

--- Mycoding/C /deleteindex.c
+++ Mycoding/C /deleteindex.c
@@ -1,1 +1,79 @@
+Ans of 1
+
+#include<stdio.h>
+
+int main ()
+{
+
+    int n,i,del,index=-1;
+
+    printf("Enter the number of the element:");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(i=0; i<n; i++)
+    {
+
+        printf("Element %d :",i+1);
+        scanf("%d",&arr[i]);
+
+    }
+
+    printf("Enter the number you want to delete :");
+    scanf("%d",&del);
+
+    for(i=0; i<n; i++)
+    {
+
+        if(arr[i]==del)
+        {
+
+            index=i;
+
+            break;
+        }
+
+        else
+        {
+            index=-1;
+        }
+
+
+    }
+
+    if(index==-1)
+    {
+
+        printf("Not found!");
+    }
+
+    else
+    {
+
+        for(i=index; i<n-1; i++)
+        {
+
+            arr[i]=arr[i+1];
+        }
+        n--;
+    }
+
+    printf("The elements are now \n");
+
+
+    for(i=0; i<n; i++)
+    {
+
+        printf("%d",arr[i]);
+¬¬
+
+¬¬
+
+    }
+
+
+    return 0;
+
+}
 
